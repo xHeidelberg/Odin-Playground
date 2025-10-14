47b54ddcacdc12e9ee0486430c41786ecb42ac1a
Laptop-Connection <167631120+xHeidelberg@users.noreply.github.com>
2025-10-15 00:52:51 +0800

    added Sample 1 and Sample 2
    await in Sample 3

--- day4/index.js
+++ day4/index.js
@@ -1,6 +1,7 @@
+// Sample 1
 let strong = document.querySelector('strong');
 let input = document.querySelector('#inputOne');
-let button = document.querySelector('#clicking');
+let button = document.querySelector('#clickingOne');
 button.addEventListener('click', onClick);
 
 function inputInOne()
@@ -23,4 +24,32 @@ function onClick()
     }
 }
 
+// Sample 2 - Alert with Click Counter not Saved
 
+let announceInput = document.querySelector('#sample2Input');
+let buttonTwo = document.querySelector('#announceBtn');
+buttonTwo.addEventListener('click', onClickAnnounce); 
+let resultTwo = document.querySelector('#samp2Result');
+let span = document.querySelector('span');
+let a = 0;
+
+
+function spanTwo()
+{
+    span.textContent = `${a++}`;
+    return;
+}
+
+function onClickAnnounce()
+{
+    const blank2Check = announceInput.value;
+    if (blank2Check === null || blank2Check === "")
+    {
+        resultTwo.textContent = "Can\'t be Empty."
+        spanTwo();
+        return;
+    }
+    alert(`Announcement: ${blank2Check}`);
+    span.textContent = `${a++}`;
+    return;
+}

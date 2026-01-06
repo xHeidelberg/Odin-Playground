6964aecb7e5e2e3065127b17405b81d07b32726e
Ded-Laptop <167631120+xHeidelberg@users.noreply.github.com>
2026-01-06 08:55:00 +0800

    Solving Stage complete

--- jan6/index.js
+++ jan6/index.js
@@ -12,7 +12,7 @@ function multiply(num1, num2) {
 
 console.log(multiply(3, 2));
 
-// Sample 3
+// Sample 3 first letter capitalized
 function capitalize(getMe) {
     if (getMe.length === 0) return getMe;
     return getMe.charAt(0).toUpperCase() + getMe.slice(1).toLowerCase();
@@ -22,7 +22,7 @@ console.log(capitalize("HELLO WORLD"));
 console.log(capitalize("javaScript"));
 console.log(capitalize("WHATkjsdkja is yo"));
 
-// Sample 4
+// Sample 4 last of string
 function lastLetter(getLast) {
     if (getLast.length === 0) return "";
     return getLast.charAt(getLast.length - 1);
@@ -31,3 +31,30 @@ function lastLetter(getLast) {
 
 console.log(lastLetter("------------------>o"));
 console.log(lastLetter("===================================================>?>!@#>>>>>G"));
+
+// Sample 6
+/*
+    write a program that takes a user’s input and prints the numbers from one to the number the user entered.
+    However, for multiples of three print Fizz instead of the number and for the multiples of five print Buzz.
+    For numbers which are multiples of both three and five print FizzBuzz.
+
+*/
+
+let whenClick = document.getElementById("clickSix");
+whenClick.addEventListener('click', FizzBuzz);
+
+function FizzBuzz(){
+    let whatInput = document.getElementById("inputSix");
+    let nowInput = parseInt(whatInput.value);
+
+    for (let i = 1; i <= nowInput; i++){
+        if (i % 3 === 0 && i % 5 === 0) // FizzBuzz
+            console.log("FizzBuzz");
+        else if (i % 5 === 0) // Buzz
+            console.log("Buzz");
+        else if (i % 3 === 0) // Fizz
+            console.log("Fizz");
+        else
+            console.log(i); //
+    }
+}

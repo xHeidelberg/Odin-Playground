aa2c1fd246db22b959090fa631ed6c77988127fe
Ded-Laptop <167631120+xHeidelberg@users.noreply.github.com>
2026-02-08 21:04:25 +0800

    add roleback for exercise

---
+++ DOM/index.js
@@ -0,0 +1,59 @@
+function sum(a, b, c) {
+    return a + b + c;
+}
+
+const number = [2, 3, 5];
+
+console.log(sum(...number));
+
+
+
+// DOM Alter
+
+const base = document.querySelector("#divTwo");
+base.style.cssText = "color: red; background: black;";
+
+base.setAttribute("divOne", "id");
+
+base.getAttribute("divOne");
+
+base.removeAttribute("divOne");
+
+// Class
+
+const baseContainer = document.querySelector("#container");
+baseContainer.classList.add("SampleCLASSADD");
+baseContainer.classList.remove("SampleCLASSADD");
+baseContainer.classList.toggle("active");
+
+// id
+document.getElementById("clicker").onclick = function theEvent() {
+    const needToChange = document.querySelector("#imHidden");
+    needToChange.style.cssText = "visibility: true; font-weight: bolder; color: red;";
+}
+// onclick DOM
+
+function clickMeDOM() {
+    //demoCLICK is in Style
+    document.getElementById("afterClick").setAttribute("id", "demoCLICK");
+}
+
+// Add and remove
+
+document.getElementById('clickerForInner').onclick = function addAndRemove() {
+    const innerBase = document.getElementById('innerHTMLsample');
+    //document.getElementById('innerToRemove').innerHTML = "";
+    const removeNow = document.getElementById('innerToRemove');
+    if (removeNow){ // if true remove else no action 
+        innerBase.removeChild(removeNow);
+    }
+
+    const contentInner = document.createElement("p");
+    contentInner.innerHTML = 'Now I\'m HERE!';
+    contentInner.style.cssText = "font-style: bolder; font-size: 50px; color: blue;";
+    document.getElementById('innerHTMLsample').appendChild(contentInner);
+
+    //contentInner.innerHTML = "<span style=\"font-style: bolder; font-size: 50px; color: blue;\">Now I\'m HERE!!</span>";
+}
+
+// Exercise for DOM
